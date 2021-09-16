652efc2c6d6078db7fe669185f59030ace711696
Chambers <46649021+connectshark@users.noreply.github.com>
2021-09-16 17:36:04 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1 class="title">{{title}}</h1>
+  <router-view></router-view>
+</template>
+
+<script>
+export default {
+  setup () {
+    return {
+      title: 'Hello'
+    }
+  }
+}
+</script>
+
+
+<style lang="scss">
+#app {
+  font-family: 'openhuninn', 'Noto Sans TC', sans-serif;
+  text-align: center;
+}
+.title{
+  font-size: 30px;
+  line-height: 1.9;
+}
+</style>
---
+++ src/views/home.vue
@@ -0,0 +1,16 @@
+<template>
+  <h2 class="title">第一頁由此開始</h2>
+</template>
+
+<script>
+export default {
+  setup () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
